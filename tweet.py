bde696492a0667963cf90e1ae079386bfad02a74
Gorka Piñol <heyslide@users.noreply.github.com>
2017-11-08 23:57:32 +0100

    All needed things for the bot
    Hoping it works a la primera

---
+++ tweet.py
@@ -0,0 +1,29 @@
+import time, tweepy, os
+from linereader import copen
+from random import randint
+
+"""tweepy doing its magic"""
+CONSUMER_KEY = os.environ['CKEY']
+CONSUMER_SECRET = os.environ['CSECRET']
+ACCESS_KEY = os.environ['AKEY']
+ACCESS_SECRET = os.environ['ASECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+lyrics = copen('frases.txt')
+lines = lyrics.count('\n')
+lastline = 0
+
+while True:
+    gottenline = randint(1, lines)
+    while gottenline == lastline:
+	       gottenline = randint(1, lines)
+    lastline = gottenline
+    tweet_text = lyrics.getline(gottenline)
+    if len(tweet_text) <= 140 and tweet_text != '\n':
+	       api.update_status(status=tweet_text)
+	       print tweet_text
+           secondsSleeping = randint(3600*3+14*60, 3600*6+28*60)
+	       time.sleep(secondsSleeping)
